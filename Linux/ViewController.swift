409974211e9d349577b1b3bfccb462db95938259
Bengin Sternas <bengin.sternas@smail.th-koeln.de>
2026-05-26 00:47:33 +0200

    Initial Commit

---
+++ Linux/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Linux
+//
+//  Created by Bengin Sternas on 26.05.26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
